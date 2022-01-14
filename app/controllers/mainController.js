f275f1b7d4980d45a99dd903f8fe566eeab564be
AgathePons <ponsagathe37@gmail.com>
2022-01-14 09:58:10 +0100

    homepage working

---
+++ app/controllers/mainController.js
@@ -0,0 +1,15 @@
+
+
+const mainController = {
+
+  async homePage(req, res) {
+    
+    res.render('home');
+  },
+
+  async displayQuiz(req, res) {
+    
+  }
+};
+
+module.exports = mainController;
---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const router = require('./app/router');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.static(path.join(__dirname, './integration')));
+
+app.use(router);
+
+app.listen(port,() => {
+  console.log(`http://localhost:${port}`);
+});
